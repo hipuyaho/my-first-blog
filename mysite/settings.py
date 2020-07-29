a07833f34acbea1a58fc07b86d0f69de8bb314a9
hipuyaho <hikosaka_0808puhipuhi@yahoo.co.jp>
2020-07-29 13:05:31 +0900

    djangogirls change

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'breakbreakdown',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
